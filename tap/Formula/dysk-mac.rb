67e334b0fb29cec33a35afd7a8fa1a58e6cbebe1
Alex Nudelman <alex.nudelman@clarityo.com>
2025-07-06 15:32:52 +0300

    done

--- tap/Formula/dysk-mac.rb
+++ tap/Formula/dysk-mac.rb
@@ -1,8 +1,8 @@
 class DyskMac < Formula
   desc "A command-line disk utility for macOS that provides detailed information about mounted disks"
-  homepage "https://github.com/yourusername/dysk-mac"
-  url "https://github.com/yourusername/dysk-mac/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "YOUR_SHA256_HERE" # Calculate this after creating the release
+  homepage "https://github.com/nudelx/dysk-mac"
+  url "https://github.com/nudelx/dysk-mac/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "2a8b121191c7a03154365524474398b5d774889456922d42ef64e6d0d838a91a"
   license "MIT"
 
   depends_on "node"
@@ -14,8 +14,15 @@ class DyskMac < Formula
     # Make the script executable
     chmod "+x", "index.js"
     
-    # Install the binary
-    bin.install "index.js" => "dysk-mac"
+    # Install all required files to libexec
+    libexec.install "index.js", "diskutil.js", "utils.js", "package.json", "node_modules"
+    
+    # Create a wrapper script
+    (bin/"dysk-mac").write <<~EOS
+      #!/bin/bash
+      exec "#{libexec}/index.js" "$@"
+    EOS
+    chmod "+x", bin/"dysk-mac"
   end
 
   test do
