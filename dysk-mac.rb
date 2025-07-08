841bc6817cfb0a34182ef420a96ff6544a615987
nudelx <nudelx@gmail.com>
2025-07-08 13:50:33 +0300

    rb

--- dysk-mac.rb
+++ dysk-mac.rb
@@ -1,8 +1,8 @@
 class DyskMac < Formula
   desc "A command-line disk utility for macOS that provides detailed information about mounted disks"
-  homepage "https://github.com/nudelx/dysk-mac"
-  url "https://github.com/nudelx/dysk-mac/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "YOUR_SHA256_HERE" # You'll need to calculate this after creating the release
+  homepage "https://github.com/nudelx/homebrew-dysk-mac"
+  url "https://github.com/nudelx/homebrew-dysk-mac/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "cbd3aeb1956ced02361575cbc223453aa18745f63eea5810f79e1eb70ebdec65" # You'll need to calculate this after creating the release
   license "MIT"
 
   depends_on "node"
